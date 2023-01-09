271161fc8280fa3fad3e306fa693505ac3b876c4
Thomas Schultz <tjschultz1@mail.mccneb.edu>
2023-01-08 18:17:31 -0600

    devops class push
    devops push

--- header.js
+++ header.js
@@ -29,6 +29,9 @@ class Header extends HTMLElement
                     <li>
                         <a href="about.html">About</a>
                     </li>
+                    <li>
+                    <a href="/web430/schultz-valuestream.html">Web 430</a>
+                    </li>
                 </ul>
             </div>
         `;
